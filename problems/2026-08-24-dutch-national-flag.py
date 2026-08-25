e95d8f2e437bb9a655cbb227ce0190b25efa5b24
H4D33D <hadeed.fawad@icloud.com>
2026-08-24 21:39:14 -0400

    Problem for 2026-08-24

--- problems/2026-08-24-dutch-national-flag.py
+++ problems/2026-08-24-dutch-national-flag.py
@@ -1,47 +1,49 @@
-"""
-Name:        H4D33D
-Problem:     Dutch National Flag Problem (a.k.a. Sort Colors)
-Difficulty:  Medium
-Date:        2026-08-24
-Time Taken:  14 mins
 
-You are given an array nums with n objects colored red, white, or blue, sort them
-in-place so that objects of the same color are adjacent, with the colors in the
-order red, white, and blue.
+# Name:        H4D33D
+# Problem:     Dutch National Flag Problem (a.k.a. Sort Colors)
+# Difficulty:  Medium
+# Date:        2026-08-24
+# Time Taken:  17 mins
 
-We will use the integers 0, 1, and 2 to represent the color red, white, and blue,
-respectively.
+########## PROBLEM ##########
 
-You must solve this problem without using the library's sort function.
+# You are given an array nums with n objects colored red, white, or blue, sort them
+# in-place so that objects of the same color are adjacent, with the colors in the
+# order red, white, and blue.
 
-Example 1:
+# We will use the integers 0, 1, and 2 to represent the color red, white, and blue,
+# respectively.
 
-Input: nums = [2,0,2,1,1,0]
-Output: [0,0,1,1,2,2]
+# You must solve this problem without using the library's sort function.
 
-Explanation:
-The array has two 0s, two 1s, and two 2s. Sorting them in-place places all 0s
-first, then all 1s, then all 2s.
+# Example 1:
 
-Example 2:
+# Input: nums = [2,0,2,1,1,0]
+# Output: [0,0,1,1,2,2]
 
-Input: nums = [2,0,1]
-Output: [0,1,2]
+# Explanation:
+# The array has two 0s, two 1s, and two 2s. Sorting them in-place places all 0s
+# first, then all 1s, then all 2s.
 
-Explanation:
-The array has one each of 0, 1, and 2, arranged in-place in the order 0, 1, 2.
+# Example 2:
 
-Constraints:
-- n == nums.length
-- 1 <= n <= 300
-- nums[i] is either 0, 1, or 2.
+# Input: nums = [2,0,1]
+# Output: [0,1,2]
 
-Follow up:
-Could you come up with a one-pass algorithm using only constant extra space?
-"""
+# Explanation:
+# The array has one each of 0, 1, and 2, arranged in-place in the order 0, 1, 2.
 
+# Constraints:
+# - n == nums.length
+# - 1 <= n <= 300
+# - nums[i] is either 0, 1, or 2.
 
-class Solution(object):
+# Follow up:
+# Could you come up with a one-pass algorithm using only constant extra space?
+
+########## SOLUTION ##########
+
+class ColorSorter(object):
     def sortColors(self, nums):
         count0 = 0
         count1 = 0
